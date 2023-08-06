29d608fcb719749a3411ea00da530b69a4781023
kaliraotaran <81376236+kaliraotaran@users.noreply.github.com>
2023-08-06 22:22:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _fontSize = 40;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: TextField(
+                  textInputAction: TextInputAction.done,
+                  style: TextStyle(fontSize: _fontSize),
+                  maxLines: null,
+                ),
+              ),
+              Slider(
+                value: _fontSize,
+                onChanged: (newSize) {
+                  setState(() {
+                    _fontSize = newSize;
+                  });
+                },
+                min: 30,
+                max: 200,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
